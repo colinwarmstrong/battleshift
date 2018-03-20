ada66bdc5d3ad9a802da24ba4483d15881ad9cca
Josh Mejia <joshua.mejia@gmail.com>
2018-03-20 15:53:25 -0600

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+player_1_board = Board.new(4)
+player_2_board = Board.new(4)
+
+sm_ship = Ship.new(2)
+md_ship = Ship.new(3)
+
+# Place Player 1 ships
+ShipPlacer.new(board: player_1_board,
+               ship: sm_ship,
+               start_space: "A1",
+               end_space: "A2").run
+
+ShipPlacer.new(board: player_1_board,
+               ship: md_ship,
+               start_space: "B1",
+               end_space: "D1").run
+
+# Place Player 2 ships
+ShipPlacer.new(board: player_2_board,
+               ship: sm_ship.dup,
+               start_space: "A1",
+               end_space: "A2").run
+
+ShipPlacer.new(board: player_2_board,
+               ship: md_ship.dup,
+               start_space: "B1",
+               end_space: "D1").run
+
+game_attributes = {
+  player_1_board: player_1_board,
+  player_2_board: player_2_board,
+  player_1_turns: 0,
+  player_2_turns: 0,
+  current_turn: "challenger"
+}
+
+game = Game.new(game_attributes)
+game.save!
