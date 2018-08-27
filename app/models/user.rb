8bb0708d6d85ce219d7c285250b400e8e31dd1fa
Colin Armstrong <colinwarmstrong@gmail.com>
2018-08-27 15:29:54 -0600

    Verify users cannot create an account without matching the password and password confirmation fields.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates_presence_of :email, :name, :password
+  validates_uniqueness_of :email
+  validates :password, confirmation: {case_sensitive: true}
 end
--- spec/features/user_can_register_spec.rb
+++ spec/features/user_can_register_spec.rb
@@ -22,5 +22,25 @@ describe 'A guest user' do
       expect(page).to have_content("Logged in as #{name}")
       expect(page).to have_content('This account has not yet been activated. Please check your email.')
     end
+
+    it 'they cannot create an account without matching password and password confirmation fields' do
+      visit root_path
+
+      click_on 'Register'
+
+      expect(current_path).to eq('/register')
+
+      name = 'John Doe'
+
+      fill_in :user_email, with: 'test@email.com'
+      fill_in :user_name, with: name
+      fill_in :user_password, with: 'password123'
+      fill_in :user_password_confirmation, with: 'password124'
+
+      click_on 'Submit'
+
+      expect(current_path).to eq(users_path)
+      expect(page).to have_content('Invalid information, please try again.')
+    end
   end
 end
