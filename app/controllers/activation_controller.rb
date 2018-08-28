fc657a1f4124f9b622a7471f319f3030b76de3be
Eliot Swank <eliotswank@gmail.com>
2018-08-27 18:06:39 -0600

    removes prys

--- app/controllers/activation_controller.rb
+++ app/controllers/activation_controller.rb
@@ -3,7 +3,6 @@ class ActivationController < ApplicationController
     @user = User.find_by_email(params[:user_email])
     @user.update(activated?: true)
     session[:user_id] = @user.id
-    # binding.pry
     flash[:notice] = "Thank you! Your account has been activated."
     redirect_to dashboard_path
   end
