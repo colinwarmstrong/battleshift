8b78748e139c909e603efe660b7bf08c41410c53
Colin Armstrong <colinwarmstrong@gmail.com>
2018-08-28 10:40:57 -0600

    Add default URL to production environment.

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module BattleshipWeb
     end
 
     config.action_mailer.delivery_method = :smtp
-    config.action_mailer.perform_deliveries = true
+    
     config.action_mailer.smtp_settings = {
       address:              'smtp.sendgrid.net',
       port:                 '587',
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,8 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.default_url_options = { :host => 'battleshift1804.herokuapp.com' }
+
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
   # `config/secrets.yml.key`.
