250ee3d83ae865b8c571aaaa1ebb9a6373f733a4
Eliot Swank <eliotswank@gmail.com>
2018-08-29 15:35:02 -0600

    clean up ships_controller

--- app/controllers/api/v1/games/ships_controller.rb
+++ app/controllers/api/v1/games/ships_controller.rb
@@ -9,14 +9,9 @@ class Api::V1::Games::ShipsController < ApiController
     end_space = ship_params[:end_space]
     ship_placer = ShipPlacer.new(board, ship, start_space, end_space)
     ship_placer.run
-    # binding.pry
     render json: game, status: 200, message: ship_placer.message(ship_params[:ship_size])
   end
   private
-
-  # def ship_count
-  #   count = ship_params[]
-  # end
 
   def ship_params
     params.permit(:ship_size, :damage, :start_space, :end_space, :game_id)
